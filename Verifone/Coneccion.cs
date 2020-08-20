b55ac85edd94a22fae5b1f2d6c8afb6b6eab809c
luis eduardo frias <luiseduardofrias27@gmail.com>
2020-08-20 15:06:26 -0400

    Initial commit

---
+++ Verifone/Coneccion.cs
@@ -0,0 +1,333 @@
+﻿namespace Verifone
+{
+    using System;
+    using System.Text;
+    using System.Net.Sockets;
+    using System.Net;
+    using System.IO;
+
+    public class Coneccion
+    {
+
+        #region Propiedades
+
+        private Socket RemoteEndPoint { get; set; }
+
+       // private Socket _Socket { get; set; }
+
+        private EndPoint ConnectRemota { get; set; }
+
+        private EndPoint ConnectLocal { get; set; }
+
+        private int Timeout = 30789;
+        byte[] SentAndReceivedBytes = new byte[1024];
+
+        private string _PathRepuesta, _nombreArchivo;
+        private StreamWriter _Answer;
+        private int _Contador;
+
+        #region Valores
+        private string ENQ = ""; // ENQ-> Enquire-> 5 -> \u0005 -> 
+        private string ACK = ""; // ACK-> Acknowledge-> 06 \u0006 -> 
+        private string EOM = ""; // EM-> End of Medium-> 19 \u0019 -> 
+        private string SYN = ""; // SYN-> Synchronous Idle-> 16 \u0016 -> 
+        private string EOT = ""; // EOT-> End Of Transmission-> 04 \u0004 -> 
+        private string CardConsultation => "CS00";
+
+        DateTime fecha = new DateTime();
+        #endregion
+
+        #endregion
+
+
+        public Coneccion(string ipLocal, int puertoLocal, string ipRemota, int puertoRemoto, string PathRepuesta,int Contador,string nombreArchivo)
+        {
+            try
+            {
+                _Contador = Contador;
+                _nombreArchivo = nombreArchivo;
+                _PathRepuesta = PathRepuesta;
+                _Answer = File.CreateText(_PathRepuesta  + "\\" + _nombreArchivo + ".txt");
+
+                ConnectRemota = new IPEndPoint(IPAddress.Parse(ipRemota), puertoRemoto);
+                ConnectLocal = new IPEndPoint(IPAddress.Parse(ipLocal), puertoLocal);
+
+            }
+            catch (Exception ex)
+            {
+                ex.Message.ToString();
+            }
+
+        }
+
+
+        //// primer metodo
+        public bool EnviarRecibirPOS(string trama)
+        {
+            try
+            {
+                if (Enquire().Equals(ACK))
+                {
+                    if (Synchronous(out SentAndReceivedBytes).Equals(EOM))
+                    {
+                        if (ECRPOS(trama))
+                        {
+
+                            return true;
+                        }
+                        else return false;
+                    }
+                    else return false;
+                }
+                else return false;
+            }
+            catch (Exception ex)
+            {
+
+                _Answer.WriteLine("Error:");
+                _Answer.WriteLine(ex.Message.ToString());
+
+                return false;
+
+            }
+            finally
+            {
+                _Answer.Close();
+                
+
+                try
+                {
+                    var g = _PathRepuesta + "\\" + _nombreArchivo + ".txt";
+                    var gg = _PathRepuesta + "\\respuesta\\" + _nombreArchivo + ".rsp";
+                    File.Move(g,gg);
+                    File.Delete(_PathRepuesta + _nombreArchivo + ".txt");
+                }
+                catch (Exception ex) { }
+            }
+        }
+
+
+        //// segundo metodo
+        private string Enquire()
+        {
+            using (RemoteEndPoint = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                RemoteEndPoint.ReceiveTimeout = Timeout;
+                RemoteEndPoint.SendTimeout = Timeout;
+                RemoteEndPoint.Connect(ConnectRemota);
+
+                var buffer = Encoding.Default.GetBytes(ENQ);
+
+                Console.WriteLine("--------------------------------------------------------------");
+                Console.WriteLine("--------------------------------------------------------------");
+                Console.WriteLine("");
+                Console.WriteLine("Trnsaccion {0} del dia.", _Contador);
+                Console.WriteLine();
+                Console.WriteLine("Enquire -> Envio {0}", ENQ);
+
+                RemoteEndPoint.Send(buffer);
+                RemoteEndPoint.Receive(buffer);
+
+                var recibedData = Encoding.ASCII.GetString(buffer); // Se espera: ACK =  = \u0006
+
+                Console.WriteLine("Enquire <- Recibi: {0}", recibedData.ToString());
+
+                if (recibedData.Equals(ACK))
+                {
+                    return ACK;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+
+        //// tercero metodo
+        private string Synchronous(out byte[] buffer)
+        {
+            using (RemoteEndPoint = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                RemoteEndPoint.ReceiveTimeout = Timeout;
+                RemoteEndPoint.SendTimeout = Timeout;
+                RemoteEndPoint.Connect(ConnectRemota);
+
+
+                buffer = Encoding.Default.GetBytes(SYN);
+
+                Console.WriteLine("SYN -> Envio {0}", SYN);
+
+                RemoteEndPoint.Send(buffer);
+                RemoteEndPoint.Receive(buffer);
+
+
+                var recibedData = Encoding.ASCII.GetString(buffer);
+
+                Console.WriteLine("SYN <- Recibi: {0} ", recibedData.ToString());
+
+                if (recibedData.Equals(EOM))
+                    return EOM;
+                else
+                    return null;
+            }
+        }
+
+
+        //// cuarto metodo
+        private bool ECRPOS(string trama)
+        {
+            string data = null;
+            char dataChar;
+            byte[] bytes = new Byte[1024];
+
+            // Create a TCP/IP socket.  
+            Socket listener = new Socket(ConnectLocal.AddressFamily,SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(ConnectLocal);
+                listener.Listen(5);
+                listener.ReceiveTimeout = Timeout;
+                listener.SendTimeout = Timeout;
+
+                Console.WriteLine("connection...");
+
+                Socket handler = listener.Accept();
+
+                data = null;
+
+
+                while (true)
+                {
+                    handler.LingerState = new LingerOption(true, 10);
+
+                    handler.Receive(SentAndReceivedBytes);
+
+                    data += Encoding.ASCII.GetString(SentAndReceivedBytes);
+
+                    Console.WriteLine("", data);
+
+                    if (data.Equals(ENQ))
+                    {
+                        SentAndReceivedBytes = (new ASCIIEncoding()).GetBytes(trama);
+
+                        Console.WriteLine(trama.ToString());
+
+                        handler.Send(SentAndReceivedBytes);
+
+                        var port = ((IPEndPoint)handler.RemoteEndPoint).Port;
+
+                        Console.WriteLine("envia Trama", Encoding.ASCII.GetString(SentAndReceivedBytes));
+
+                        handler.Receive(SentAndReceivedBytes);
+
+                        dataChar = Convert.ToChar(SentAndReceivedBytes[0]);
+
+                        Console.WriteLine("recibo ACK {0}", dataChar);
+
+                        if (dataChar.ToString().Equals(ACK))
+                        {
+                            do
+                            {
+                                Console.WriteLine("Muestra el Handler");
+                                if (handler.Connected)
+                                {
+                                    SentAndReceivedBytes = new byte[1024];
+
+                                    handler.Receive(SentAndReceivedBytes);
+
+                                    dataChar = Convert.ToChar(SentAndReceivedBytes[0]);
+
+                                    Console.WriteLine("EOT {0}", dataChar.ToString());
+
+                                    if (!dataChar.ToString().Equals(EOT))
+                                    {
+
+                                        data = Encoding.ASCII.GetString(SentAndReceivedBytes).Replace("\0", "");
+
+
+                                        Console.WriteLine("recibe Trama", data);
+
+                                        SentAndReceivedBytes = (new ASCIIEncoding()).GetBytes(ACK.ToString());
+
+                                        handler.Send(SentAndReceivedBytes);
+
+                                    }
+                                }
+
+                            } while (!dataChar.ToString().Equals(EOT));
+
+
+                            _Answer.WriteLine(data);
+                            Console.WriteLine("antes del Break: {0}", data);
+                        }
+
+                        handler.Send((new ASCIIEncoding()).GetBytes(ACK));
+
+                        Console.WriteLine("Envia ACK {0}", ACK);
+
+                        data = handler.Receive(SentAndReceivedBytes).ToString();
+
+                        Console.WriteLine("Recibe {0}", data);
+                        break;
+                    }
+                };
+
+                Console.WriteLine("Final del todo : {0}", data);
+
+
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+
+
+                _Answer.WriteLine("Error:");
+                _Answer.WriteLine(ex.ToString());
+
+
+                Acknowledge();
+
+                return false;
+            }
+            finally { listener.Close(); }
+
+        }
+
+
+        //// quinto metodo
+        private string Acknowledge()
+        {
+            using (RemoteEndPoint = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                RemoteEndPoint.ReceiveTimeout = Timeout;
+                RemoteEndPoint.SendTimeout = Timeout;
+                RemoteEndPoint.Connect(ConnectRemota);
+
+                var buffer = System.Text.Encoding.Default.GetBytes(ACK);
+
+                Console.WriteLine("ACK -> Envio {0}", ACK);
+                RemoteEndPoint.Send(buffer);
+                RemoteEndPoint.Receive(buffer);
+
+                var recibedData = System.Text.Encoding.ASCII.GetString(buffer); // Se espera: ACK =  = \u0006
+                Console.WriteLine("EOT <- Recibi: {0}", recibedData.ToString());
+
+                if (recibedData.Equals(ACK))
+                    return ACK;
+                else
+                    return null;
+            }
+
+        }
+    
+
+    }
+}
+
---
+++ Verifone/Program.cs
@@ -0,0 +1,284 @@
+﻿namespace Verifone
+{
+    using System;
+    using System.IO;
+    using System.Security.Cryptography.X509Certificates;
+
+    class Program
+    {
+        private static string _IPMaquina, _IPVerifone, _Datos, _Entero, _path, _Transaccion, _FechaHora, _TipoVerifone;
+        private static int _PuertoMaquina, _PuertoVerifone,_Monto, _Itbis, _Tikes, _OtroImpuesto, _Contador, _Conteotransacciones = 0, _Diferido;
+        private static bool _procesar, _Exception;
+        private static FileInfo _NombreArchivo;
+
+        static void Main(string[] args)
+        {
+            _path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase).Substring(6);
+
+            do
+            {
+
+                #region initializing variables
+
+                _IPMaquina = string.Empty;
+                _IPVerifone = string.Empty;
+                _PuertoMaquina = 0;
+                _PuertoVerifone = 0;
+                _FechaHora = string.Empty;
+                _TipoVerifone = string.Empty;
+
+                _Transaccion = string.Empty;
+                _Datos = string.Empty;
+                _Entero = string.Empty;
+
+                _procesar = false;
+
+                _Diferido = 0;
+                _Monto = 0;
+                _Itbis = 0;
+                _Tikes = 0;
+                _OtroImpuesto = 0;
+                _Contador = 1;
+
+                #endregion
+
+                try
+                {
+                    do
+                    {
+                        try
+                        {
+                            DirectoryInfo Archivo = new DirectoryInfo(_path + "\\entradas\\");
+
+                            _NombreArchivo = Archivo.GetFiles("*.txt")[0];
+
+                            _Datos = File.ReadAllText(_path + "\\entradas\\" + _NombreArchivo.Name);
+
+                            _procesar = true;
+
+                            File.Move(_path + "\\entradas\\" + _NombreArchivo.Name, _path + "\\PeticionesProcesadas\\" + _NombreArchivo.Name);
+
+                            File.Delete(_path + "\\entradas\\" + _NombreArchivo.Name);
+
+                        }
+                        catch (Exception ex) { }
+
+                    } while (_procesar == false);
+
+                    char[] b = new char[_Datos.Length];
+
+                    using (StringReader sr = new StringReader(_Datos))
+                    {
+                        sr.Read(b, 0, _Datos.Length);
+
+                        foreach (var caracteres in b)
+                        {
+                            if (caracteres.Equals(';'))
+                            {
+                                _Contador++;
+                                _Entero = string.Empty;
+                            }
+                            else
+                            {
+                                if(!caracteres.Equals('\0') && !caracteres.Equals('\n'))
+                                {
+                                    switch (_Contador)
+                                    {
+                                        case 1:
+                                            {
+                                                _Transaccion += Convert.ToString(caracteres).Trim();
+
+                                                break;
+                                            }
+                                        case 2:
+                                            {
+                                                _IPMaquina += Convert.ToString(caracteres).Trim();
+
+                                                break;
+                                            }
+                                        case 3:
+                                            {
+                                                _Entero += Convert.ToString(caracteres).Trim();
+                                                _PuertoMaquina = Convert.ToInt32(_Entero);
+
+                                                break;
+                                            }
+                                        case 4:
+                                            {
+                                                _IPVerifone += Convert.ToString(caracteres).Trim();
+
+                                                break;
+                                            }
+                                        case 5:
+                                            {
+                                                _Entero += Convert.ToString(caracteres).Trim();
+                                                _PuertoVerifone = Convert.ToInt32(_Entero);
+
+                                                break;
+                                            }
+                                        case 6:
+                                            {
+                                                _Entero += Convert.ToString(caracteres).Trim();
+                                                _Monto = Convert.ToInt32(_Entero);
+                                                break;
+                                            }
+                                        case 7:
+                                            {
+                                                _Entero += Convert.ToString(caracteres).Trim();
+                                                _Itbis = Convert.ToInt32(_Entero);
+                                                break;
+                                            }
+                                        case 8:
+                                            {
+                                                _Entero += Convert.ToString(caracteres).Trim();
+                                                _OtroImpuesto += Convert.ToInt32(Convert.ToString(caracteres).Trim());
+                                                break;
+                                            }
+                                        case 9:
+                                            {
+                                                _Entero += Convert.ToString(caracteres).Trim();
+                                                _Tikes = Convert.ToInt32(_Entero);
+                                                break;
+                                            }
+                                        case 10:
+                                            {
+                                                _Entero += Convert.ToString(caracteres).Trim();
+                                                _Diferido += Convert.ToInt32(Convert.ToString(caracteres).Trim());
+                                                break;
+                                            }
+                                        case 11:
+                                            {
+                                                _FechaHora += Convert.ToString(caracteres).Trim();
+                                                break;
+                                            }
+                                        case 12:
+                                            {
+                                                _TipoVerifone += Convert.ToString(caracteres).Trim();
+                                                break;
+                                            }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    execcion(ex.Message);
+
+                    _Exception = true;
+                }
+
+                if(verificacion())
+                {
+                    if (_TipoVerifone == "CARDNET")
+                    {
+                        _Conteotransacciones++;
+
+                        Coneccion con = new Coneccion(_IPMaquina, _PuertoMaquina, _IPVerifone, _PuertoVerifone, _path, _Conteotransacciones, _NombreArchivo.Name.Substring(0, Convert.ToInt32(_NombreArchivo.Name.Length) - 4));
+
+                        if (_Transaccion == "CU00")
+                        {
+                            if (!con.EnviarRecibirPOS(_Transaccion + "" + _Monto + "" + _Itbis + "" + _OtroImpuesto + "" + _Tikes + "" + _Diferido))
+                                _Conteotransacciones--;
+                        }
+                        else if (_Transaccion.Equals("CN00") || _Transaccion.Equals("CN01"))
+                        {
+                            if (!con.EnviarRecibirPOS(_Transaccion + "" + _Monto + "" + _Itbis + "" + _OtroImpuesto + "" + _Tikes))
+                                _Conteotransacciones--;
+                        }
+
+                        _Exception = false;
+                    }
+                    else if (_TipoVerifone == "VISANET")
+                    {
+
+                    }
+                }
+                else
+                {
+                    execcion();
+                }
+                    
+
+               
+
+            } while (true);
+           
+        }
+
+        private static bool verificacion()
+        {
+            try
+            {
+                string HoraFecha = DateTime.Now.ToString("dd,MM,yyyy") + "-" + DateTime.Now.ToString("HH:mm:ss", System.Globalization.CultureInfo.InvariantCulture);
+                string dataFH1 = HoraFecha.Substring(0, 10);
+                string dataFH2 = _FechaHora.Substring(0, 10);
+
+                if (dataFH1 == dataFH2)
+                {
+                    dataFH1 = HoraFecha.Substring(11);
+                    dataFH2 = _FechaHora.Substring(11);
+
+                    if (dataFH1.Substring(0, 2) == dataFH2.Substring(0, 2))
+                    {
+                        if (int.Parse(dataFH1.Substring(3, 2)) <= (int.Parse(dataFH2.Substring(3, 2)) + 2))
+                        {
+                            if (!_Exception)
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        private static void execcion(string ex)
+        {
+            Console.WriteLine("Error1 :" + ex);
+
+            var ruta = _path + "\\respuesta\\" + (_NombreArchivo.Name.Substring(0, Convert.ToInt32(_NombreArchivo.Name.Length) - 4)) + ".txt";
+
+            using (StreamWriter Answer = File.CreateText(ruta))
+            {
+                Answer.WriteLine("Error:");
+                Answer.WriteLine(ex);
+            }
+
+            File.Move(ruta, ruta.Substring(0, (Convert.ToInt32(ruta.Length)) - 4) + ".rsp");
+        }
+
+        private static void execcion()
+        {
+            var ruta = _path + "\\respuesta\\" + (_NombreArchivo.Name.Substring(0, Convert.ToInt32(_NombreArchivo.Name.Length) - 4)) + ".txt";
+
+            using (StreamWriter Answer = File.CreateText(ruta))
+            {
+                Answer.WriteLine("Error:");
+            }
+
+            File.Move(ruta, ruta.Substring(0, (Convert.ToInt32(ruta.Length)) - 4) + ".rsp");
+        }
+    }
+}
